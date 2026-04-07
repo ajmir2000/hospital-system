4c1f885c1639d08631ec6c4cce1b1bcbec236931
Mohammad Ajmir <ajmerfaqiri14.af@gmail.com>
2026-04-07 21:43:03 +0430

    send user data to convex

--- app/components/ConvexClientProvider.tsx
+++ app/components/ConvexClientProvider.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
-import { ConvexReactClient } from "convex/react";
-import { ClerkProvider, useAuth } from "@clerk/nextjs";
-import React from "react";
+import { Authenticated, ConvexReactClient, useMutation } from "convex/react";
+import { ClerkProvider, useAuth, useUser } from "@clerk/nextjs";
+import React, { useEffect } from "react";
+import { api } from "@/convex/_generated/api";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
@@ -11,10 +12,27 @@ function ConvexClientProvider({ children }: { children: React.ReactNode }) {
     <ClerkProvider
       publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}>
       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        <Authenticated>
+          <UserSync />
+        </Authenticated>
         {children}
       </ConvexProviderWithClerk>
     </ClerkProvider>
   );
 }
 
+function UserSync() {
+  const { user } = useUser();
+  const createUser = useMutation(api.patients.createUser);
+  useEffect(() => {
+    if (user) {
+      createUser({
+        name: user.fullName,
+        email: user.emailAddresses[0].emailAddress,
+      });
+    }
+  }, [user, createUser]);
+
+  return null;
+}
 export default ConvexClientProvider;
